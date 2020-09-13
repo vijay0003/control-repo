49131c7fc5139163e4fc5181e279ce4847e9c2b2
vijay0003 <49054184+vijay0003@users.noreply.github.com>
2020-09-13 15:56:49 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,4 +4,9 @@ node default {
     content => 'This is a readme',
     owner   => 'root',
   }
+  $minute = fqdn_rand(9)
+  cron { 'restart_sshd':
+    command => '/bin/systemctl restart sshd',
+    user    => 'root',
+    minute  => $minute,
 }
