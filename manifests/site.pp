6dbfbf720e752669f98e28d229df17773b43e8bf
vijay0003 <49054184+vijay0003@users.noreply.github.com>
2020-09-13 16:06:59 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,10 @@
+$minute = fqdn_rand(9)
 node default {
   file { '/root/README':
     ensure  => file,
     content => 'This is a readme',
     owner   => 'root',
   }
-  $minute = fqdn_rand(9)
   cron { 'restart_sshd':
     command => '/bin/systemctl restart sshd',
     user    => 'root',
