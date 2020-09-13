841e4dede26e981f9e957cdfcd6a0387e3cbe12f
vijay0003 <49054184+vijay0003@users.noreply.github.com>
2020-09-13 18:16:15 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,6 @@
 #mod 'puppet-cron', '2.0.0',
-$minute = fqdn_rand(59,'ds_alertd')
-$hour = fqdn_rand(23,'ds_alertd')
+$minute = fqdn_rand(59,'sshd')
+$hour = fqdn_rand(23,'sshd')
 node default {
   file { '/root/README':
     ensure  => file,
